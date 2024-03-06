78be34e46728883d010fd094444a55fb84e1fbdc
LocTM <tranminhloc.anhk@gmail.com>
2024-03-06 13:57:33 +0700

    add Class Glasses to model

---
+++ src/main/java/com/codegym/model/Glasses.java
@@ -0,0 +1,71 @@
+package com.codegym.model;
+
+public class Glasses {
+    //    Glasses (id, code, price, color, description, img)
+    private Long id;
+    private String code;
+    private int price;
+    private String color;
+    private String description;
+    private String img;
+
+    public Glasses() {
+    }
+
+    public Glasses(Long id, String code, int price, String color, String description, String img) {
+        this.id = id;
+        this.code = code;
+        this.price = price;
+        this.color = color;
+        this.description = description;
+        this.img = img;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
