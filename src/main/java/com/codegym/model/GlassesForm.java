cb2f7cd49f38b9ebd9f643bca4757e1ae29ee247
LocTM <tranminhloc.anhk@gmail.com>
2024-03-06 14:29:35 +0700

    add GlassesForm to model

---
+++ src/main/java/com/codegym/model/GlassesForm.java
@@ -0,0 +1,81 @@
+package com.codegym.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.*;
+
+
+public class GlassesForm {
+//    Glasses (id, code, price, color, description, img)
+
+    public GlassesForm() {
+    }
+
+    public GlassesForm(int id, String code, String price, String color, String description, MultipartFile img) {
+        this.id = id;
+        this.code = code;
+        this.price = price;
+        this.color = color;
+        this.description = description;
+        this.img = img;
+    }
+
+    public MultipartFile getImg() {
+        return img;
+    }
+
+    public void setImg(MultipartFile img) {
+        this.img = img;
+    }
+
+    private int id;
+    private String code;
+    private String price;
+    private String color;
+    private String description;
+    private MultipartFile img;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
